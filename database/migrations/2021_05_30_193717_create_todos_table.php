870776c2150d3a8f7376873411791731389421f5
Mikhail Aloshyn <mikhail.alyoshyn@gmail.com>
2021-05-31 23:10:21 +0300

    migration: created migration for todo, todostatus

---
+++ database/migrations/2021_05_30_193717_create_todos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('status');
+            $table->text('title');
+            $table->timestamp('start_datetime');
+            $table->timestamp('end_datetime')->nullable();
+            $table->timestamp('archived_datetime')->nullable();
+            $table->timestamps();
+            $table->foreign('status')->references('id')->on('todo_statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+};
