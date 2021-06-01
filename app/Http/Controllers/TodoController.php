120219688a313706b0339e0fd95b3b03a25cfa33
Mikhail Aloshyn <mikhail.alyoshyn@gmail.com>
2021-06-02 00:17:26 +0300

    feat: added todo_list view,route,controller method

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -5,20 +5,21 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use App\Models\Todo;
+use App\Models\TodoStatus;
 
 class TodoController extends Controller
 {
-    //
+    use ValidatesRequests;
+    
     public function index()
     {
-        echo 'Index controller';
-        foreach(Todo::all() as $todo)
-        {
-            echo '<pre>';
-            print_r($todo->title);
-            echo '</pre>';
-        }
-        
+        return view('todo_list', [
+            'todos' => Todo::all()->filter(function($value)
+            {
+                return $value->status !== TodoStatus::where('status', 'archived')->first()->id;
+            }),
+            'statuses' => TodoStatus::all()]);
     }
 }
